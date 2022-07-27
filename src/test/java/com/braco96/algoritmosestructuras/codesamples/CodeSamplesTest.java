4bd48b494b6406ca74fe2d08c5965d87448ece79
braco96 <bravocolladoluis@gmail.com>
2022-07-27 11:13:00 +0000

    commit 01/08

---
+++ src/test/java/com/braco96/algoritmosestructuras/codesamples/CodeSamplesTest.java
@@ -0,0 +1,25 @@
+package com.braco96.algoritmosestructuras.codesamples;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.braco96.algoritmosestructuras.codesamples.CodeSamples;
+
+public class CodeSamplesTest {
+  CodeSamples codeSamples = new CodeSamples();
+
+  @Test
+  public void testFibonacci() {
+    System.out.println(codeSamples.fibonacci(46));
+  }
+
+  @Test
+  public void testFibonacciDynamicProgramming() {
+    System.out.println(codeSamples.fibonacciMemo(46));
+    System.out.println(codeSamples.fibonacciBottomUp(46));
+
+    assertEquals(1836311903, codeSamples.fibonacciMemo(46));
+    assertEquals(1836311903, codeSamples.fibonacciBottomUp(46));
+  }
+}
